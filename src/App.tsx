3fd7f15f079cdc53ec927b4d55a2d714640040f4
Ruma303 <matteoh8@hotmail.it>
2024-07-07 16:53:19 +0200

    screen ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import './index.css'
+import "./index.css";
 
 function App() {
-
   return (
     <>
-      <div className="w-screen h-screen p-4 flex flex-col items-center
-        bg-white xxs:bg-pink-200 xs:bg-sky-200 sm:bg-slate-300 md:bg-slate-400
-        lg:bg-slate-500 xl:bg-slate-600 2xl:bg-slate-700 3xl:bg-slate-800
-      ">
-        <main className='bg-purple-100 xxs:bg-white max-w-screen-lg px-4 p-3 rounded-md flex flex-col gap-4'>
-            <h1 className="text-4xl font-semibold text-red-500">Configurazioni di Tailwind</h1>
-            <p className="text-lg text-gray-500">In questa repo impareremo a personalizzare Tailwind per il nostro progetto, usando una semplice app di React.js</p>
-            <p>
-            </p>
+      <div
+        className="w-screen h-screen p-4 flex flex-col items-center
+            bg-white xxs:bg-emerald-200 xs:bg-sky-200 sm:bg-pink-300 md:bg-slate-400
+            lg:bg-amber-200 xl:bg-slate-200 2xl:bg-purple-200 3xl:bg-indigo-300
+        ">
 
+        <main className="max-w-screen-lg px-4 p-3 rounded-md flex flex-col gap-4 border-3 border-gray-200
+        phone:bg-white tablet:bg-sky-300 desktop:bg-emerald-200
+        ">
+          <h1 className="text-4xl font-semibold text-red-500 md:text-white lg:text-slate-700">
+            Configurazioni di Tailwind
+          </h1>
+          <p className="text-lg text-gray-500 sm:text-gray-700 md:text-gray-200 lg:text-gray-600">
+            In questa repo impareremo a personalizzare Tailwind per il nostro
+            progetto, usando una semplice app di React.js
+          </p>
+          <p></p>
         </main>
-       </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,67 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{html,js,ts,jsx,tsx}",
-    ],
-    theme: {
-        screens: {
-            'xxs': '320px',
-            'xs': '480px',
-            'sm': '640px',
-            'md': '768px',
-            'lg': '1024px',
-            'xl': '1280px',
-            '2xl': '1536px',
-            '3xl': '1920px',
+    import defaultTheme from 'tailwindcss/defaultTheme';
+
+    /** @type {import('tailwindcss').Config} */
+    export default {
+        content: [
+            "./index.html",
+            "./src/**/*.{html,js,ts,jsx,tsx}",
+        ],
+        theme: {
+            extend: {
+                screen: {
+                    'portrait': {'raw': '(min-height: 800px) and (orientation: portrait)'},
+                    'lg': '992px',
+                    'xl': '1280px',
+                    '2xl': '1440px',
+                    '3xl': '1920px',
+                },
+            },
+            screens: {
+                'xxs': '320px',
+                'xs': '480px',
+                ...defaultTheme.screens,
+                'phone' : {'max': '767px'},
+                'tablet': {'min': '768px', 'max': '1023px'},
+                'desktop': {'min': '1024px', 'max': '1279px'},
+            },
+            extend: {
+                screen: {
+                    'lg': '992px',
+                    'xl': '1280px',
+                    '2xl': '1440px',
+                    '3xl': '1920px',
+                },
+            },
+            /* colors: {
+                transparent: 'transparent',
+                black: '#000',
+                white: '#fff',
+                gray: {
+                    50: '#f9fafb',
+                    100: '#f7fafc',
+                    200: '#edf2f7',
+                    300: '#e2e8f0',
+                    400: '#cbd5e0',
+                    500: '#a0aec0',
+                    600: '#718096',
+                    700: '#4a5568',
+                    800: '#2d3748',
+                    900: '#1a202c',
+                },
+                slate: {
+                    50: '#f9fafb',
+                    100: '#f4f5f7',
+                    200: '#e5e7eb',
+                    300: '#d2d6dc',
+                    400: '#9fa6b2',
+                    500: '#6b7280',
+                    600: '#4b5563',
+                    700: '#374151',
+                    800: '#252f3f',
+                    900: '#161e2e',
+                },
+
+            } */
         },
-    },
-}
+    }
 
